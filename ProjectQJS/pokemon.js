9f2cad40a971dc92c3bfaf67a86ff8f320432fec
LethiciaDuncan <lduncan@student.neumont.edu>
2023-05-08 18:09:23 -0600

    Home Page design
    Current home page design

--- ProjectQJS/pokemon.js
+++ ProjectQJS/pokemon.js
@@ -1,49 +1,124 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button ,TextInput ,Image, ScrollView,Text, Picker, View  } from 'react-native';
-
+import { StyleSheet, Button,Text,View  } from 'react-native';
 export default function App() {
-  return (
-    <>
+return (
+     <>
     <View style={styles.container}>
-    <Text>Project</Text>
-    <Text>Q.J.S.L</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.submit}>
-      <Button title = "Begin AR"/>
-      <StatusBar style="auto" />
-    </View>
-    <View style={styles.info}>
-         <Text style={styles.innerText}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-   
-    </>
-  
 
+      <Text>Pokemon</Text>
+      <StatusBar style="auto" />
+       <View style={styles.ball}> 
+       <View style={styles.line}>
+       <View style={styles.button}>
+        <Button 
+        backgroundColor= '#fff'
+        position= 'absolute'
+        width = '50'
+        height= '30'
+        borderRadius= '20'
+        left= '130'
+        />
+        </View>
+         <View style={styles.ball2}> 
+         <View style={styles.ball3}> 
+         <View style={styles.ball4}> 
+         <View style={styles.ball5}>
+         <View style={styles.ball6}>
+         </View>
+         </View>
+         </View>
+         </View>
+         </View>
+       </View>
+       </View>
+      </View>
+    </>
+
+    
   );
+
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: .50,
-    backgroundColor: '#f12f',
+
+    flex: 1,
+    backgroundColor: '#CDCDCD',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  submit: {
-    flex : .05,
+  },ball: {
+    backgroundColor: '#f03',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    justifyContent: 'center',
+
+  },ball2: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
- },
-  info:{
-    flex:.50,
-    backgroundColor:'#000',
-    alignItems:'center',
-    justifyContent: 'center',
+    width: "101%",
+    height: 30,
+    borderRadius: 5,
+    left: -1,
+    bottom:-30,
+    position: 'absolute',
+
   },
-  innerText:{
-color:'black'
+  ball3: {
+    backgroundColor: '#fff',
+    width: "93%",
+    height: 50,
+    borderRadius: 5,
+    left: 10,
+    bottom:-30,
+    position: 'absolute',
+
   },
+  ball4: {
+    backgroundColor: '#fff',
+    width: "91%",
+    height: 45,
+    borderRadius: 5,
+    left: 13,
+    bottom:-30,
+    position: 'absolute',
+
+  },
+  ball5: {
+    backgroundColor: '#fff',
+    width: "81%",
+    height: 40,
+    left: 25,
+    bottom:-30,
+    borderRadius: 5,
+    position: 'absolute',
+
+  },
+  ball6: {
+    backgroundColor: '#fff',
+    width: "59%",
+    height: 30,
+    left: 43,
+    bottom:-15,
+    borderRadius: 5,
+    position: 'absolute',
+
+  },
+  line: {
+    backgroundColor: '#000',
+    width: "100%",
+    height: 30,
+    zIndex:2,
+  },
+  button: {
+    backgroundColor: '#fff',
+    position: 'absolute',
+    width : 50,
+    height: 30,
+    borderRadius: 20,
+    left: 130
+  }
+
 });
