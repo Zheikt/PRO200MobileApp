3bf39f4c4aae7f8e6f2280008770274aa1e0127b
qhough <qhough@student.neumont.edu>
2023-05-17 13:48:02 -0600

    home page
    added the home page (hopefully)

--- project_qjsl_expo/components/pokemon.js
+++ project_qjsl_expo/components/pokemon.js
@@ -1,39 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, View, Image } from 'react-native';
-export default function HomePage() {
-  return (
-    <>
-      <View style={styles.container}>
+import React, {useEffect} from 'react';
+import { StyleSheet, Button,Text,View, Image,ImageBackground } from 'react-native';
+import {Audio} from 'expo-av';
 
-        <Image style={styles.image} source={require("../pictures/title.png")} />
+export default function App() {
 
-        <StatusBar style="auto" />
-        <View style={styles.ball}>
-          <View style={styles.line}>
-            <View style={styles.button}>
-              <Text style={styles.font}>Go</Text>
-              <Button
-                title='Go'
-                color='#fff'
-                position='absolute'
-                width='50'
-                height='30'
-                borderRadius='20'
-                left='130'
-              />
-            </View>
-            <View style={styles.ball2}>
-              <View style={styles.ball3}>
-                <View style={styles.ball4}>
-                  <View style={styles.ball5}>
-                    <View style={styles.ball6}>
-                    </View>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </View>
+useEffect(()=>{
+const playSound = async () =>{
+  const soundObj = new Audio.Sound();
+  try {
+    await soundObj.loadAsync(require("mp3/pokemon.mp3"));
+    soundObj.setIsLoopingAsync(true);
+    await soundObj.playAsync();
+  }catch(error){
+    console.warn('Error playing background music:', error);
+  }
+};
+playSound();
+return async () => {
+  const soundObj = new Audio.Sound();
+  await soundObj.stopAsync();
+  await soundObj.unloadAsync(); 
+};
+}, []);
+
+ 
+return (
+     <>
+    <View style={styles.container}>
+    <ImageBackground source = {require("./pictures/testbackground.jpg")} resizeMode="cover" style={styles.imagebackground}/>
+
+    <Image style={styles.image} source = {require("./pictures/title.png")}/>
+
+      <StatusBar style="auto" />
+       <View style={styles.ball}> 
+       <View style={styles.line}>
+       <View style={styles.button}>
+       <Text style = {styles.font}>Go</Text>
+        <Button 
+        title = 'Go'
+        color = '#fff'
+        position= 'absolute'
+        width = '50'
+        height= '30'
+        borderRadius= '20'
+        left= '130'
+        />
         </View>
+         <View style={styles.ball2}> 
+         <View style={styles.ball3}> 
+         <View style={styles.ball4}> 
+         <View style={styles.ball5}>
+         <View style={styles.ball6}>
+         </View>
+         </View>
+         </View>
+         </View>
+         </View>
+       </View>
+       </View>
       </View>
     </>
 
@@ -42,6 +67,21 @@ export default function HomePage() {
 
 }
 
+function myAudio(){
+
+const [sound, setSound] = useState('');
+
+  async function bigSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('./mp3/pokemon.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+}
 
 
 const styles = StyleSheet.create({
@@ -51,7 +91,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#CDCDCD',
     alignItems: 'center',
     justifyContent: 'center',
-  }, ball: {
+  },ball: {
     backgroundColor: '#f03',
     alignItems: 'center',
     width: 300,
@@ -59,13 +99,13 @@ const styles = StyleSheet.create({
     borderRadius: 150,
     justifyContent: 'center',
 
-  }, ball2: {
+  },ball2: {
     backgroundColor: '#fff',
     width: "101%",
     height: 30,
     borderRadius: 5,
     left: -1,
-    bottom: -30,
+    bottom:-30,
     position: 'absolute',
 
   },
@@ -75,7 +115,7 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 5,
     left: 10,
-    bottom: -30,
+    bottom:-30,
     position: 'absolute',
 
   },
@@ -85,7 +125,7 @@ const styles = StyleSheet.create({
     height: 45,
     borderRadius: 5,
     left: 13,
-    bottom: -30,
+    bottom:-30,
     position: 'absolute',
 
   },
@@ -94,7 +134,7 @@ const styles = StyleSheet.create({
     width: "81%",
     height: 40,
     left: 25,
-    bottom: -30,
+    bottom:-30,
     borderRadius: 5,
     position: 'absolute',
 
@@ -104,7 +144,7 @@ const styles = StyleSheet.create({
     width: "59%",
     height: 30,
     left: 43,
-    bottom: -15,
+    bottom:-15,
     borderRadius: 5,
     position: 'absolute',
 
@@ -113,27 +153,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     width: "100%",
     height: 30,
-    zIndex: 2,
+    zIndex:2,
   },
   button: {
     backgroundColor: '#fff',
     position: 'absolute',
-    width: 50,
+    width : 50,
     height: 30,
     borderRadius: 20,
     left: 130
   },
-  image: {
+  image:{ 
     flex: 1,
     width: 300,
     height: 200,
     resizeMode: 'contain'
 
-  },
+},
+imagebackground:{ 
+    flex: 1,
+    width: 320,
+    height: 590,
+    resizeMode: 'contain'
 
-  font: {
-    fontFamily: 'Times New Roman',
-    fontWeight: 'bold'
-  }
+},
+
+font:{
+ fontFamily: 'Times New Roman',
+ fontWeight: 'bold'
+}
 
 });
